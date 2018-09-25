b9c837b3ed6e7597fe7f37fbc9b09ddcec5a87c2
Alice Bob <alice@eni.fr>
2018-09-25 11:26:06 +0200

    ok

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,10 +1,11 @@
 var should = require("should"); 
-var User = function(name){
-    this.nickname = "toto";
-         this.name = name; };
-      describe("Checking if the user is created correctly", function(){
-         it("should create the user with the correct name", function(){
-                  var tom = new User("tom");
-                   tom.name.should.be.equal("tommy");
+var User = function(name, nickname){
+         this.name = name; 
+        this.nickname = nickname; }; 
+     describe("Checking if the user is created correctly", function(){     it("should create the user with the correct name", 
+     function(){         
+         var tom = new User("tom", "tommy");
+                  tom.name.should.be.equal("tom");
+                tom.nickname.should.be.equal("tommy");
+             });
          });
-});
