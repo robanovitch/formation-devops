7fab4ced7e8753d744d2daa1587043218a042811
Alice Bob <alice@eni.fr>
2018-09-25 10:26:31 +0200

    Update compare tom name

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,9 +1,8 @@
-var should = require("should"); 
-var User = function(name){
-         this.name = name; }; 
-     describe("Checking if the user is created correctly", function(){     it("should create the user with the correct name", 
-     function(){         
-         var tom = new User("tom");
-                  tom.name.should.be.equal("tom");
-             });
+var should = require("should"); var User = function(name){
+         this.name = name; };
+      describe("Checking if the user is created correctly", function(){
+         it("should create the user with the correct name", function(){
+                  var tom = new User("tom");
+                   tom.name.should.be.equal("tommy");
          });
+});
